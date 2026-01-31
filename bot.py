ae0f8e81c34efa4a96035badf83a49fe843cb053
Your Name <you@example.com>
2026-01-31 22:04:39 +0300

    City_full_1

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 from handlers import start, trip
+from db.database import init_db
 
 
 async def main():
+    # 🔹 ВАЖНО: инициализация БД ДО запуска бота
+    init_db()
+
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
@@ -15,6 +18,7 @@ async def main():
     dp.include_router(trip.router)
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
